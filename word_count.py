eeec0ddd4249971c703cd2cfa1c47f91104f6b98
SS-CC-YY <songchen@oregonstate.edu>
2021-05-15 15:49:48 +0800

    make some changes

--- word_count.py
+++ word_count.py
@@ -2,14 +2,14 @@ import collections
 
 def word_count(in_put):
     s_list = in_put.split(' ')
-    [s_list.remove(item) for item in s_list if item in ',.']
+    [s_list.remove(item) for item in s_list if item in ",."]
     count = collections.Counter(s_list)
-    return count
+    return len(count.keys())
 
 def main():
     sentence = input("Enter a sentence: ")
     count = word_count(sentence)
-    print("The numer of words: ", len(count.keys()))
+    print("The numer of words: ", count)
 
 if __name__ == "__main__":
     main()
